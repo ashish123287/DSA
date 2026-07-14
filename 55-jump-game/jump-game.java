640d1ca908c030b8c1b9145eed287a1fad93eddf
Ashish Prajapati <ashishrkt21@gmail.com>
2026-07-14 19:41:18 +0530

    Time: 1 ms (99.94%) | Memory: 47.9 MB (63.07%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -1,18 +1,10 @@
 class Solution {
     public boolean canJump(int[] nums) {
         int n = nums.length;
-        boolean[] check = new boolean[n];
-        check[n-1] = true;
-        for(int i = n-2; i >= 0; i--){
-            if(nums[i] == 0) continue;
-            int k = 1;
-            while(i+k < n && k <= nums[i]){
-                if(check[i+k++]){
-                    check[i] = true;
-                    break;
-                }
-            }
+        int close = n-1;    // most close true index from where we can reach end index
+        for(int i = n-1; i >= 0; i--){
+            if(close <= i+nums[i]) close = i;
         }
-        return check[0];
+        return close == 0;
     }
 }
